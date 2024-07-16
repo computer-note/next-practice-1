2aeaa3377d3760d4d80aec1b9ea0498ee3258fe4
computer-note <planomenes@gmail.com>
2024-07-16 20:28:37 +0900

    update : ISG(ISR) 방식 페이지 렌더링 사용
    특이사항: counter 변수를 사용해서 서버가 페이지를 빌드하는 시점을 console.log로 출력

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,53 @@
-export default function Home() {
-  return <main>HOME</main>;
+let counter = 1;
+export default async function Home() {
+  const response = await fetch(`https://randomuser.me/api`, {
+    next: {
+      revalidate: 5,
+    },
+  });
+  const { results } = await response.json();
+  const user = results[0];
+
+  console.log(++counter);
+
+  return (
+    <main>
+      <h1>Incremental Static Generation/Regeneration</h1>
+      <h2>counter:{counter}</h2>
+      <div className='bg-slate-400'>
+        <div className='flex gap-8'>
+          <div>
+            <h2 className='text-xl font-bold'>
+              {user.name.title}. {user.name.first} {user.name.last}
+            </h2>
+            <p className='text-gray-600'>{user.email}</p>
+
+            <div className='mt-4'>
+              <p>
+                <span className='font-bold'>전화번호 : </span>
+                {user.phone}
+              </p>
+              <p>
+                <span className='font-bold'>휴대전화번호 : </span>
+                {user.cell}
+              </p>
+              <p>
+                <span className='font-bold'>사는 곳 : </span>
+                {user.location.city}, {user.location.country}
+              </p>
+              <p>
+                <span className='font-bold'>등록일자 : </span>
+                {new Date(user.registered.date).toLocaleDateString()}
+              </p>
+
+              <p>
+                <span className='font-bold'>생년월일 : </span>
+                {new Date(user.dob.date).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
