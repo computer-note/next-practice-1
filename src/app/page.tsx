25c541b5d11b76d48baa644b55882a6223a663b0
computer-note <planomenes@gmail.com>
2024-07-17 13:57:34 +0900

    update : CSR로 페이지를 제공하는 방식 테스트

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,74 @@
+'use client';
+
+import { useEffect, useState } from 'react';
+
+let counter = 1;
+
 export default function Home() {
-  return <main>HOME</main>;
+  const [user, setUser] = useState<any>(null);
+
+  useEffect(() => {
+    async function loadUser() {
+      const response = await fetch(`https://randomuser.me/api`);
+      const data = await response.json();
+      setUser(data.results[0]);
+    }
+
+    console.log(counter++);
+
+    loadUser();
+  }, []);
+
+  console.log(counter++);
+
+  if (!user) {
+    return (
+      <main>
+        <h1>Loading data...</h1>
+        <h2>counter:{counter}</h2>
+      </main>
+    );
+  }
+
+  return (
+    <main>
+      <h1>Server Side Rendering</h1>
+      <h2>counter:{counter}</h2>
+
+      <div className='bg-slate-400'>
+        <div className='flex gap-8'>
+          <div>
+            <h2 className='text-xl font-bold'>
+              {user.name.title}. {user.name.first} {user.name.last}
+            </h2>
+            <p className='text-gray-600'>{user.email}</p>
+
+            <div className='mt-4'>
+              <p>
+                <span className='font-bold'>전화번호 : </span>
+                {user.phone}
+              </p>
+              <p>
+                <span className='font-bold'>휴대전화번호 : </span>
+                {user.cell}
+              </p>
+              <p>
+                <span className='font-bold'>사는 곳 : </span>
+                {user.location.city}, {user.location.country}
+              </p>
+              <p>
+                <span className='font-bold'>등록일자 : </span>
+                {new Date(user.registered.date).toLocaleDateString()}
+              </p>
+
+              <p>
+                <span className='font-bold'>생년월일 : </span>
+                {new Date(user.dob.date).toLocaleDateString()}
+              </p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
 }
